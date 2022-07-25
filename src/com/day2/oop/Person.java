3c9f91341e4bf63904a04d861a8560358817383e
JWahed <jak.clev@gmail.com>
2022-07-25 16:54:01 +0100

    Created Person.java and Runner.java
    Initial commit

---
+++ src/com/day2/oop/Person.java
@@ -0,0 +1,33 @@
+package com.day2.oop;
+
+import java.util.List;
+
+public class Person {
+
+    // Create a Person class modelling name, age and job title variables.
+    private String name;
+    private int age;
+    private String jobTitle;
+
+    public String getName() {
+        return name;
+    }
+
+    public Person(String name, int age, String jobTitle) {
+        this.name = name;
+        this.age = age;
+        this.jobTitle = jobTitle;
+    }
+    // Create a method to return all three of these in a formatted string.
+    public String toString() {
+        return String.format("Name: %s\n Age: %d\n Job Title: %s\n", this.name, this.age, this.jobTitle);
+    }
+    public static Person findByName(String name, List<Person> persons) {
+        for (Person p: persons) {
+            if (p.getName().equals(name)) {
+                return p;
+            }
+        }
+        return null;
+    }
+}
