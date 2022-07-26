afc988177436a7ed823077d3add33de660eb243d
JWahed <jak.clev@gmail.com>
2022-07-26 11:45:40 +0100

    Update Person.java
    created getters and setters and added comments

--- src/com/day2/oop/Person.java
+++ src/com/day2/oop/Person.java
@@ -9,19 +9,27 @@ public class Person {
     private int age;
     private String jobTitle;
 
-    public String getName() {
-        return name;
-    }
+    // Getters and Setters
+    public String getName() { return name; }
+    public int getAge() { return age; }
+    public String getJobTitle() { return jobTitle; }
+    public void setName(String n) { this.name = n; }
+    public void setAge(int i) { this.age = i; }
+    public void setJobTitle(String j) { this.name = j; }
 
+    // All args constructor
     public Person(String name, int age, String jobTitle) {
         this.name = name;
         this.age = age;
         this.jobTitle = jobTitle;
     }
+
     // Create a method to return all three of these in a formatted string.
     public String toString() {
         return String.format("Name: %s\nAge: %d\nJob Title: %s\n", this.name, this.age, this.jobTitle);
     }
+
+    // Create a method that can search for a specific Person by their name.
     public static Person findByName(String name, List<Person> persons) {
         for (Person p: persons) {
             if (p.getName().equalsIgnoreCase(name)) {
