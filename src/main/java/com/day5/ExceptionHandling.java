87ac3b740d2b7f894b8e5d3fcc4d25dbd053c494
JWahed <jak.clev@gmail.com>
2022-07-28 14:09:19 +0100

    Create ExceptionHandling.java

---
+++ src/main/java/com/day5/ExceptionHandling.java
@@ -0,0 +1,20 @@
+package com.day5;
+
+public class ExceptionHandling {
+
+    public static int divide(int x, int y) {
+        int result = 0;
+
+        try {
+            result = x / y;
+        } catch (ArithmeticException ae) {
+            System.out.println("Can not divide by 0");
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(divide(1, 0));
+    }
+}
