fb622ab7f4e77766892183fec42b2f3010f024bd
JWahed <jak.clev@gmail.com>
2022-07-29 15:48:44 +0100

    Update Map.java
    removed a variable and altered methods to match

--- src/main/java/com/day6/library/Map.java
+++ src/main/java/com/day6/library/Map.java
@@ -3,17 +3,8 @@ package com.day6.library;
 
 public class Map extends Items {
 
-    private String name;
     private String country;
 
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public String getCountry() {
         return country;
@@ -26,16 +17,12 @@ public class Map extends Items {
 
     public Map() {}
 
-    public Map(String name, String country) {
-        this.name = name;
+    public Map(String country) {
         this.country = country;
     }
 
     @Override
     public String toString() {
-        return "Maps{" +
-                "Name of Map: '" + name + '\'' +
-                ", Country: '" + country + '\'' +
-                '}';
+        return "This is a map of: " + country;
     }
 }
